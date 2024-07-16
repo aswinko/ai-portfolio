cd9bea2ee1915cb64990d518b456b745cbbb784f
Aswin K O <aswinko479@gmail.com>
2024-07-16 19:34:04 +0530

    minor change in loadData route

--- app/api/loadData/route.ts
+++ app/api/loadData/route.ts
@@ -1,11 +1,21 @@
 import { createCollection, loadData } from "@/database/loadDB";
 
 export default async function handler(req: any, res: any) {
-    if (req.method === 'POST') {
-      await createCollection();
-      await loadData();
-      res.status(200).json({ message: 'Data loaded successfully' });
-    } else {
-      res.status(405).json({ message: 'Method Not Allowed' });
-    }
+  if (req.method === "POST") {
+    await createCollection();
+    await loadData();
+    res.status(200).json({ message: "Data loaded successfully" });
+  } else {
+    res.status(405).json({ message: "Method Not Allowed" });
   }
+}
+
+export async function POST(req: any, res: any) {
+  try {
+    await createCollection();
+    await loadData();
+    res.status(200).json({ message: "Data loaded successfully" });
+  } catch (error) {
+    console.log(error);
+  }
+}
